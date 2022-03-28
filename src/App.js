e99670dc0aa810162f6324948b7422076e5009f3
Khanz1998 <zahrakhan282@gmail.com>
2022-03-28 11:16:46 +0100

    Added bootstrap and logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <img src={logo} className="App-logo img-fluid shadow mt-5" alt="logo" />
       </header>
     </div>
   );
