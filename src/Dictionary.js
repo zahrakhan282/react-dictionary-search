2e1694525fab313b81843ea2873dc6147c484bed
Khanz1998 <zahrakhan282@gmail.com>
2022-03-30 12:37:44 +0100

    Added API documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,10 +11,8 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
-
+    // documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
